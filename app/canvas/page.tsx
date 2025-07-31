c0d5549322e350c5c48998ba1f7af33a343e220b
Attato <rde8519@mail.ru>
2025-07-31 23:34:32 +0300

    stretched the canvas to full screen

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 export default function Canvas() {
 	return (
 		<div className="flex flex-col items-center justify-center gap-2 h-screen">
-			<Link href="/">go to home</Link>
+			<Link href="/" className="absolute top-4 left-4">
+				go to home
+			</Link>
 
-			<canvas className="border border-white"></canvas>
+			<canvas className="w-full h-full"></canvas>
 		</div>
 	);
 }
