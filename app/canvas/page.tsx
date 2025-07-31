e18c0c3964f455d0c4225ca171425ce37590d317
Attato <rde8519@mail.ru>
2025-07-31 23:43:06 +0300

    prepared the canvas for work

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -1,13 +1,26 @@
+'use client';
+
 import Link from 'next/link';
+import { useEffect, useRef } from 'react';
 
 export default function Canvas() {
+	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (!canvas) return;
+
+		const ctx = canvas.getContext('2d');
+		if (!ctx) return;
+	}, []);
+
 	return (
 		<div className="flex flex-col items-center justify-center gap-2 h-screen">
 			<Link href="/" className="absolute top-4 left-4">
 				go to home
 			</Link>
 
-			<canvas className="w-full h-full"></canvas>
+			<canvas ref={canvasRef} className="w-full h-full"></canvas>
 		</div>
 	);
 }
