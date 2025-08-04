b18c7276060b7b4fe87b745f8298978d312cbb0c
Attato <rde8519@mail.ru>
2025-08-04 15:27:04 +0300

    scale -> zoomLevel

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -1,25 +1,26 @@
 'use client';
 
-import Link from 'next/link';
 import { useRef } from 'react';
+
+import Link from 'next/link';
+
 import { useCanvasControls } from '@/canvas/hooks/useCanvasControls';
 import { useCanvasRenderer } from '@/canvas/hooks/useCanvasRenderer';
 
 export default function Canvas() {
-	const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-	const { offset, scale, selectionStart, selectionEnd } =
-		useCanvasControls(canvasRef);
+    const { offset, zoomLevel, selectionStart, selectionEnd } = useCanvasControls(canvasRef);
 
-	useCanvasRenderer(canvasRef, offset, scale, selectionStart, selectionEnd);
+    useCanvasRenderer(canvasRef, offset, zoomLevel, selectionStart, selectionEnd);
 
-	return (
-		<div className="flex flex-col items-center justify-center gap-2 h-screen">
-			<Link href="/" className="absolute top-4 left-4">
-				go to home
-			</Link>
-			<div className="absolute bottom-4 left-4">scale: {scale.toFixed(2)}</div>
-			<canvas ref={canvasRef} className="w-full h-full" />
-		</div>
-	);
+    return (
+        <div className="flex flex-col items-center justify-center gap-2 h-screen">
+            <Link href="/" className="absolute top-4 left-4">
+                go to home
+            </Link>
+            <div className="absolute bottom-4 left-4">{zoomLevel.toFixed(2)}x</div>
+            <canvas ref={canvasRef} className="w-full h-full" />
+        </div>
+    );
 }
