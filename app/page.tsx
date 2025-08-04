9862098edbba282ff59b406bf3a3dcb4e9be3c73
Attato <rde8519@mail.ru>
2025-08-04 15:20:05 +0300

    prettier init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 
 export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center gap-2 h-screen">
-			<h1 className="text-4xl">Hello, Sequoia!</h1>
-			<Link href="/canvas">go to canvas</Link>
-		</div>
-	);
+    return (
+        <div className="flex flex-col items-center justify-center gap-2 h-screen">
+            <h1 className="text-4xl">Hello, Sequoia!</h1>
+            <Link href="/canvas">go to canvas</Link>
+        </div>
+    );
 }
