b769a4397ae18c60d7094221ca7d5b44406acb00
Attato <rde8519@mail.ru>
2025-08-17 15:28:16 +0300

    + Inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
 import { ReactNode } from 'react';
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
-	title: 'Sequoia',
-	description: 'Designed to create many different trees',
+    title: 'Sequoia',
+    description: 'Designed to create many different trees',
 };
 
+const inter = Inter({ subsets: ['latin'], weight: ['100', '400', '700'] });
+
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: ReactNode;
+    children: ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
 }
