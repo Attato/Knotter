9417f1875c71d707da39b7647bea352b48ba17dd
Attato <rde8519@mail.ru>
2025-08-24 00:29:01 +0300

    home page minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <div className="flex flex-col items-center justify-center gap-2 h-screen">
-            <h1 className="text-4xl">Hello, Sequoia!</h1>
-            <Link href="/canvas">go to canvas</Link>
+            <Link href="/canvas" className="text-6xl font-extrabold">
+                В РАЗРАБОТКЕ...
+            </Link>
         </div>
     );
 }
