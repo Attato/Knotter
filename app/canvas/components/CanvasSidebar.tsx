a5824988fbda2c702c2ef761c03d0faa0afd9cbc
Attato <rde8519@mail.ru>
2025-08-28 23:46:51 +0300

    add search input to CanvasSidebar

--- app/canvas/components/CanvasSidebar.tsx
+++ app/canvas/components/CanvasSidebar.tsx
@@ -9,11 +9,12 @@ import CanvasSidebarList from '@/canvas/components/CanvasSidebarList';
 import { useCanvasStore } from '@/canvas/store/сanvasStore';
 import { handleAddNode } from '@/canvas/utils/handleAddNode';
 
-import { PanelRight, Plus, Home } from 'lucide-react';
+import { PanelRight, Plus, Home, Search } from 'lucide-react';
 
 export default function CanvasSidebar() {
     const { nodes, setNodes } = useCanvasStore();
     const [isOpen, setIsOpen] = useState(false);
+    const [filterText, setFilterText] = useState('');
 
     const addNode = () => {
         const newNodes = handleAddNode(nodes);
@@ -34,18 +35,31 @@ export default function CanvasSidebar() {
             </button>
 
             <div className="flex flex-col h-full overflow-hidden">
-                <div className="m-1">
+                <div className="flex items-center gap-2 m-1">
                     <button
                         onClick={addNode}
                         className="w-fit bg-[#151515] hover:bg-[#1a1a1a] p-2 rounded-md transition cursor-pointer"
                     >
                         <Plus size={16} />
                     </button>
+
+                    <div className="relative flex-1">
+                        <input
+                            type="text"
+                            placeholder="Поиск..."
+                            value={filterText}
+                            onChange={(e) => setFilterText(e.target.value)}
+                            className="w-full h-8  bg-[#151515] text-white placeholder-[#888] pl-3 pr-9 py-2 text-sm rounded-md focus:outline-none"
+                        />
+                        <span className="absolute right-3 top-1/2 -translate-y-1/2 text-[#888]">
+                            <Search size={14} />
+                        </span>
+                    </div>
                 </div>
 
                 <hr className="border-b-0 border-[#1a1a1a]" />
 
-                <CanvasSidebarList />
+                <CanvasSidebarList filterText={filterText} />
 
                 <hr className="border-b-0 border-[#1a1a1a]" />
 
--- app/canvas/components/CanvasSidebarList.tsx
+++ app/canvas/components/CanvasSidebarList.tsx
@@ -6,8 +6,11 @@ import { updateNodeSelection } from '@/canvas/utils/updateNodeSelection';
 
 import { GripVertical } from 'lucide-react';
 
-export default function CanvasSidebarList() {
+export default function CanvasSidebarList({ filterText }: { filterText: string }) {
     const { nodes, edges, selectedNodeIds, setSelectedNodeIds, setNodes } = useCanvasStore();
+
+    const filteredNodes = nodes.filter((node) => node.name.toLowerCase().includes(filterText.toLowerCase()));
+
     const [draggingIndex, setDraggingIndex] = useState<number | null>(null);
 
     const dragNodeRef = useRef<Node | null>(null);
@@ -23,6 +26,8 @@ export default function CanvasSidebarList() {
         const listItems = Array.from(e.currentTarget.children) as HTMLElement[];
 
         listItems.forEach((li, idx) => {
+            if (li.parentElement !== e.currentTarget) return;
+
             const rect = li.getBoundingClientRect();
 
             if (e.clientY > rect.top && e.clientY < rect.bottom && idx !== draggingIndex) {
@@ -42,54 +47,57 @@ export default function CanvasSidebarList() {
 
     return (
         <ul className="flex-1 overflow-y-auto" onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
-            {nodes.map((node: Node, index: number) => {
-                const isSelected = selectedNodeIds.includes(node.id);
-                const nodeEdges = edges.filter((edge) => edge.from === node.id || edge.to === node.id);
+            {filteredNodes.length === 0 ? (
+                <li className=" flex justify-center p-4 m-1 text-[#888] text-sm">Ничего не найдено.</li>
+            ) : (
+                filteredNodes.map((node: Node, index: number) => {
+                    const isSelected = selectedNodeIds.includes(node.id);
+                    const nodeEdges = edges.filter((edge) => edge.from === node.id || edge.to === node.id);
 
-                return (
-                    <li
-                        key={node.id}
-                        onMouseDown={() => handleMouseDown(index)}
-                        onClick={(e) => {
-                            const newSelectedIds = updateNodeSelection(nodes, selectedNodeIds, node.id, e);
-                            setSelectedNodeIds(newSelectedIds);
-                        }}
-                        className={`flex flex-col px-4 py-2 m-1 rounded-md cursor-pointer tabular-nums transition-all duration-150 ${
-                            isSelected ? 'bg-[#388bfd1a]' : 'bg-[#151515] hover:bg-[#1a1a1a]'
-                        }`}
-                    >
-                        <div className="flex items-center justify-between">
-                            <EditableName
-                                name={node.name}
-                                isSelected={isSelected}
-                                onChange={(newName) =>
-                                    setNodes(nodes.map((n) => (n.id === node.id ? { ...n, name: newName } : n)))
-                                }
-                            />
+                    return (
+                        <li
+                            key={node.id}
+                            onMouseDown={() => handleMouseDown(index)}
+                            onClick={(e) => {
+                                const newSelectedIds = updateNodeSelection(nodes, selectedNodeIds, node.id, e);
+                                setSelectedNodeIds(newSelectedIds);
+                            }}
+                            className={`flex flex-col px-4 py-2 m-1 rounded-md cursor-pointer tabular-nums transition-all duration-150 ${
+                                isSelected ? 'bg-[#388bfd1a]' : 'bg-[#151515] hover:bg-[#1a1a1a]'
+                            }`}
+                        >
+                            <div className="flex items-center justify-between">
+                                <EditableName
+                                    name={node.name}
+                                    isSelected={isSelected}
+                                    onChange={(newName) =>
+                                        setNodes(nodes.map((n) => (n.id === node.id ? { ...n, name: newName } : n)))
+                                    }
+                                />
+                                <GripVertical color="#999" size={16} />
+                            </div>
 
-                            <GripVertical color="#999" size={16} />
-                        </div>
+                            {nodeEdges.length > 0 && (
+                                <ul className="ml-4 mt-1 space-y-1">
+                                    {nodeEdges.map((edge) => {
+                                        const fromNode = nodes.find((n) => n.id === edge.from);
+                                        const toNode = nodes.find((n) => n.id === edge.to);
 
-                        {nodeEdges.length > 0 && (
-                            <ul className="ml-4 mt-1 space-y-1">
-                                {nodeEdges.map((edge) => {
-                                    const fromNode = nodes.find((n) => n.id === edge.from);
-                                    const toNode = nodes.find((n) => n.id === edge.to);
-
-                                    return (
-                                        <li
-                                            key={`${edge.from}-${edge.to}`}
-                                            className="text-[12px] text-[#aaa] cursor-pointer"
-                                        >
-                                            {`Связь: ${fromNode?.name} → ${toNode?.name}`}
-                                        </li>
-                                    );
-                                })}
-                            </ul>
-                        )}
-                    </li>
-                );
-            })}
+                                        return (
+                                            <li
+                                                key={`${edge.from}-${edge.to}`}
+                                                className="text-[12px] text-[#aaa] cursor-pointer"
+                                            >
+                                                {`Связь: ${fromNode?.name} → ${toNode?.name}`}
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
+                            )}
+                        </li>
+                    );
+                })
+            )}
         </ul>
     );
 }
