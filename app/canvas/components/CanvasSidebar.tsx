ba1eb6657a776922db338f3e7ee35dab1bb0e0cd
Attato <rde8519@mail.ru>
2025-08-30 22:41:56 +0300

    make CanvasSidebar always visible

--- app/canvas/components/CanvasSidebar.tsx
+++ app/canvas/components/CanvasSidebar.tsx
@@ -9,11 +9,10 @@ import CanvasSidebarList from '@/canvas/components/CanvasSidebarList';
 import { useCanvasStore } from '@/canvas/store/сanvasStore';
 import { handleAddNode } from '@/canvas/utils/handleAddNode';
 
-import { PanelRight, Plus, Home, Search } from 'lucide-react';
+import { Plus, Home, Search } from 'lucide-react';
 
 export default function CanvasSidebar() {
     const { nodes, setNodes } = useCanvasStore();
-    const [isOpen, setIsOpen] = useState(false);
     const [filterText, setFilterText] = useState('');
 
     const addNode = () => {
@@ -22,18 +21,7 @@ export default function CanvasSidebar() {
     };
 
     return (
-        <aside
-            className={`absolute top-0 right-0 h-full w-120 border-l border-[#1a1a1a] bg-[#0f0f0f] select-none transform transition-transform duration-300 ease-in-out ${
-                isOpen ? 'translate-x-0' : 'translate-x-full'
-            }`}
-        >
-            <button
-                onClick={() => setIsOpen(!isOpen)}
-                className="absolute -left-[38px] top-4 z-20 border border-r-0 border-[#1a1a1a] bg-[#0f0f0f] hover:bg-[#1a1a1a] text-white p-2 rounded-l-lg shadow-md transition-all cursor-pointer"
-            >
-                <PanelRight size={20} />
-            </button>
-
+        <aside className={`h-screen w-120 border-l border-[#1a1a1a] bg-[#0f0f0f] select-none duration-300 ease-in-out`}>
             <div className="flex flex-col h-full overflow-hidden">
                 <div className="flex items-center gap-2 m-1">
                     <button
@@ -49,7 +37,7 @@ export default function CanvasSidebar() {
                             placeholder="Поиск..."
                             value={filterText}
                             onChange={(e) => setFilterText(e.target.value)}
-                            className="w-full h-8  bg-[#151515] text-white placeholder-[#888] pl-3 pr-9 py-2 text-sm rounded-md focus:outline-none"
+                            className="w-full h-8 bg-[#151515] text-white placeholder-[#888] pl-3 pr-9 py-2 text-sm rounded-md focus:outline-none"
                         />
                         <span className="absolute right-3 top-1/2 -translate-y-1/2 text-[#888]">
                             <Search size={14} />
