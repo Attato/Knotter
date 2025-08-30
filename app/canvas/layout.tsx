4177ba326b692580410e84e45b39bace0f26dc77
Attato <rde8519@mail.ru>
2025-08-30 19:54:37 +0300

    + flex style

--- app/canvas/layout.tsx
+++ app/canvas/layout.tsx
@@ -5,7 +5,7 @@ import CanvasSidebar from '@/canvas/components/CanvasSidebar';
 
 export default function CanvasLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="relative h-screen w-screen overflow-hidden">
+        <div className="flex h-screen w-screen overflow-hidden">
             <main className="h-full w-full">{children}</main>
 
             <CanvasSidebar />
