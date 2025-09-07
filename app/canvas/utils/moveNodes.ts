a3c4e0cc14e4254a162a622da8b88b08579cbe15
Attato <rde8519@mail.ru>
2025-09-07 23:53:28 +0300

    simplify moveNodes logic

--- app/canvas/utils/moveNodes.ts
+++ app/canvas/utils/moveNodes.ts
@@ -1,5 +1,4 @@
-import { Position } from '@/canvas/canvas.types';
-import { Node } from '@/canvas/canvas.types';
+import { Position, Node } from '@/canvas/canvas.types';
 
 export function moveNodes(
     nodes: Node[],
@@ -11,21 +10,17 @@ export function moveNodes(
     const { x: dx, y: dy } = dragDelta;
 
     return nodes.map((node) => {
-        if (selectedNodeIds.includes(node.id)) {
-            const initialPos = initialNodePositions.get(node.id);
-            if (!initialPos) return node;
+        if (!selectedNodeIds.includes(node.id)) return node;
 
-            const newX = initialPos.x + dx;
-            const newY = initialPos.y + dy;
+        const initialPos = initialNodePositions.get(node.id);
+        if (!initialPos) return node;
 
-            return {
-                ...node,
-                position: {
-                    x: Math.round(newX / nodeMoveStep) * nodeMoveStep,
-                    y: Math.round(newY / nodeMoveStep) * nodeMoveStep,
-                },
-            };
-        }
-        return node;
+        return {
+            ...node,
+            position: {
+                x: Math.round((initialPos.x + dx) / nodeMoveStep) * nodeMoveStep,
+                y: Math.round((initialPos.y + dy) / nodeMoveStep) * nodeMoveStep,
+            },
+        };
     });
 }
