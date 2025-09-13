fbc5c7742f3e1d56cdea9080021440b08d0b90d6
Attato <rde8519@mail.ru>
2025-09-13 14:40:41 +0300

    add MAX_UNDO_STEPS

--- app/canvas/constants.ts
+++ app/canvas/constants.ts
@@ -2,6 +2,8 @@ export const MIN_ZOOM = 0.1;
 export const MAX_ZOOM = 10;
 export const INITIAL_ZOOM = 1;
 
+export const MAX_UNDO_STEPS = 50;
+
 export const NODE_SIZE = 40;
 export const NODE_MOVE_MIN_STEP = 1;
 export const NODE_MOVE_MAX_STEP = 50;
