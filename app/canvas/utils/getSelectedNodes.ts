ba3dff3f75c0191be3815774f5d7815c602f8f01
Attato <rde8519@mail.ru>
2025-09-13 15:03:05 +0300

    getSelectedNodes init

---
+++ app/canvas/utils/getSelectedNodes.ts
@@ -0,0 +1,5 @@
+import { CanvasItem, Node } from '../canvas.types';
+import { getNodes } from './getNodes';
+
+export const getSelectedNodes = (items: CanvasItem[], selectedIds: string[]): Node[] =>
+    getNodes(items).filter((node) => selectedIds.includes(node.id));
