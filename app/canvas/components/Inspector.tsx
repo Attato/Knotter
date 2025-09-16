189680c2ea3a0c1d01310a1af7faae25568194c2
Attato <rde8519@mail.ru>
2025-09-16 21:07:12 +0300

    show item coordinates with AxisInfo component

--- app/canvas/components/Inspector.tsx
+++ app/canvas/components/Inspector.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react';
 import { CanvasItem, NodeType } from '@/canvas/canvas.types';
 import { useCanvasStore } from '@/canvas/store/сanvasStore';
 import { Octagon, Circle, Squircle, Hexagon, Triangle, Diamond, Dot, type LucideIcon } from 'lucide-react';
-
 import { handleItemNameChange } from '@/canvas/utils/handleItemNameChange';
 
 type InspectorProps = {
@@ -23,11 +22,21 @@ const ICONS: Record<NodeType, LucideIcon> = {
     point: Dot,
 };
 
+const AxisInfo = ({ label, value }: { label: string; value: number }) => (
+    <div className="flex gap-2 bg-card px-2 py-1 rounded-md items-center w-full">
+        <div className="w-5 flex justify-center">{label}</div>
+        <div className="w-px bg-border h-6" />
+        <span className="select-text">{value.toFixed(0)}</span>
+    </div>
+);
+
 export default function Inspector({ item }: InspectorProps) {
     const { items, setItems } = useCanvasStore();
     const [name, setName] = useState(item.name);
     const [nodeType, setNodeType] = useState<NodeType | null>(item.kind === 'node' ? item.type : null);
     const [isOpen, setIsOpen] = useState(false);
+
+    const currentItem = items.find((i) => i.id === item.id) || item;
 
     useEffect(() => {
         setName(item.name);
@@ -48,42 +57,49 @@ export default function Inspector({ item }: InspectorProps) {
     };
 
     return (
-        <div className="flex items-center m-2 relative">
-            {nodeType && (
-                <div className="relative">
-                    <button
-                        onClick={() => setIsOpen((prev) => !prev)}
-                        className={`h-8 px-2 flex items-center gap-1 justify-center rounded-l-md ${isOpen ? 'bg-ui' : 'bg-card'} hover:bg-ui border-r border-border cursor-pointer`}
-                    >
-                        <ShapePreview type={nodeType} size={20} />
-                    </button>
+        <div className="flex flex-col m-2 relative gap-2">
+            <div className="flex">
+                {nodeType && (
+                    <>
+                        <button
+                            onClick={() => setIsOpen((prev) => !prev)}
+                            className={`h-8 px-2 flex items-center gap-1 justify-center rounded-l-md ${isOpen ? 'bg-ui' : 'bg-card'} hover:bg-ui border-r border-border cursor-pointer`}
+                        >
+                            <ShapePreview type={nodeType} size={20} />
+                        </button>
 
-                    {isOpen && (
-                        <div className="absolute top-full left-0 mt-1 bg-card rounded-md shadow-lg z-50 flex flex-col">
-                            {NODE_TYPES.map((type) => (
-                                <button
-                                    key={type}
-                                    onClick={() => handleTypeChange(type)}
-                                    className="w-full flex items-center gap-2 px-2 py-1 hover:bg-ui cursor-pointer rounded-md"
-                                >
-                                    <ShapePreview type={type} size={24} />
-                                </button>
-                            ))}
-                        </div>
-                    )}
-                </div>
-            )}
+                        {isOpen && (
+                            <div className="absolute top-full left-0 mt-1 bg-card rounded-md shadow-lg z-50 flex flex-col">
+                                {NODE_TYPES.map((type) => (
+                                    <button
+                                        key={type}
+                                        onClick={() => handleTypeChange(type)}
+                                        className="w-full flex items-center gap-2 px-2 py-1 hover:bg-ui cursor-pointer rounded-md"
+                                    >
+                                        <ShapePreview type={type} size={24} />
+                                    </button>
+                                ))}
+                            </div>
+                        )}
+                    </>
+                )}
 
-            <input
-                type="text"
-                value={name}
-                onChange={(e) => {
-                    setName(e.target.value);
-                    handleItemNameChange(item, e.target.value);
-                }}
-                className="w-full h-8 bg-card text-foreground placeholder-gray pl-3 pr-9 text-sm rounded-r-md focus:outline-none"
-                placeholder="Название"
-            />
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => {
+                        setName(e.target.value);
+                        handleItemNameChange(item, e.target.value);
+                    }}
+                    className="w-full h-8 bg-card text-foreground placeholder-gray pl-3 pr-9 text-sm rounded-r-md focus:outline-none"
+                    placeholder="Название"
+                />
+            </div>
+
+            <div className="flex justify-between gap-2">
+                <AxisInfo label="X" value={currentItem.position.x} />
+                <AxisInfo label="Y" value={currentItem.position.y} />
+            </div>
         </div>
     );
 }
