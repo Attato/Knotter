9707c0b0b1a3dc54b77a56603aab83daaa15c533
Attato <rde8519@mail.ru>
2025-09-17 15:50:13 +0300

    delete breadcrumbs

--- app/canvas/utils/handleItemNameChange.ts
+++ app/canvas/utils/handleItemNameChange.ts
@@ -2,9 +2,8 @@ import { CanvasItem } from '@/canvas/canvas.types';
 import { useCanvasStore } from '@/canvas/store/сanvasStore';
 
 export const handleItemNameChange = (item: CanvasItem, newName: string) => {
-    const { items, setItems, setBreadcrumbs } = useCanvasStore.getState();
+    const { items, setItems } = useCanvasStore.getState();
 
     const updatedItems = items.map((i) => (i.id === item.id ? { ...i, name: newName } : i));
     setItems(updatedItems);
-    setBreadcrumbs([{ label: 'Канвас' }, { label: newName }]);
 };
--- app/canvas/utils/openInspector.ts
+++ app/canvas/utils/openInspector.ts
@@ -2,8 +2,7 @@ import { CanvasItem } from '@/canvas/canvas.types';
 import { useCanvasStore } from '@/canvas/store/сanvasStore';
 
 export const openInspector = (item: CanvasItem) => {
-    const { setInspectorItem, setBreadcrumbs } = useCanvasStore.getState();
+    const { setInspectorItem } = useCanvasStore.getState();
 
     setInspectorItem(item);
-    setBreadcrumbs([{ label: 'Канвас' }, { label: item.name }]);
 };
