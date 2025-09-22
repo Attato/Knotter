bd6b8335d60b7a5f6879268b6c8247d635aeb03b
Attato <rde8519@mail.ru>
2025-09-22 22:29:42 +0300

    getCanvasCenter init

---
+++ app/canvas/utils/getCanvasCenter.ts
@@ -0,0 +1,12 @@
+import { useCanvasStore } from '@/canvas/store/сanvasStore';
+import { Position } from '@/canvas/canvas.types';
+
+export function getCanvasCenter(canvas: HTMLCanvasElement): Position {
+    const { offset, zoomLevel, invertY } = useCanvasStore.getState();
+    const rect = canvas.getBoundingClientRect();
+
+    const centerX = (rect.width / 2 - offset.x) / zoomLevel;
+    const centerY = invertY ? (rect.height / 2 - offset.y) / zoomLevel : (rect.height / 2 - offset.y) / zoomLevel;
+
+    return { x: centerX, y: centerY };
+}
