4b2c20e6844b75297b5e0839fd452c647c7cedc5
Attato <rde8519@mail.ru>
2025-09-24 01:57:03 +0300

    add submenus to contextMenuItem

--- app/components/UI/ContextMenuItem.tsx
+++ app/components/UI/ContextMenuItem.tsx
@@ -1,26 +1,50 @@
 'use client';
 
-import React, { FC, ReactNode, MouseEvent } from 'react';
+import React, { FC, ReactNode, MouseEvent, useState } from 'react';
+
+import { ChevronRight } from 'lucide-react';
 
 interface ContextMenuItemProps {
-    onClick: (e?: MouseEvent<HTMLButtonElement>) => void;
+    onClick?: (e?: MouseEvent<HTMLButtonElement>) => void;
     children: ReactNode;
     disabled?: boolean;
     shortcut?: string;
+    submenu?: ReactNode;
 }
 
-export const ContextMenuItem: FC<ContextMenuItemProps> = ({ onClick, children, disabled = false, shortcut }) => {
-    return (
-        <button
-            className={`flex justify-between items-center px-3 py-1 bg-card hover:bg-ui w-full text-left cursor-pointer ${disabled ? 'opacity-40' : ''}`}
-            onClick={(e) => onClick?.(e)}
-            disabled={disabled}
-        >
-            <span>{children}</span>
+export const ContextMenuItem: FC<ContextMenuItemProps> = ({ onClick, children, disabled = false, shortcut, submenu }) => {
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
 
-            {shortcut && (
-                <span className={` ${disabled ? 'opacity-40' : 'text-gray'} ml-2 text-xs select-none`}>{shortcut}</span>
+    return (
+        <div className="relative" onMouseEnter={() => setIsSubmenuOpen(true)} onMouseLeave={() => setIsSubmenuOpen(false)}>
+            <button
+                className={`flex justify-between items-center px-3 py-1 bg-card hover:bg-ui w-full text-left cursor-pointer ${
+                    disabled ? 'opacity-40 cursor-not-allowed' : ''
+                }`}
+                onClick={(e) => !submenu && onClick?.(e)}
+                disabled={disabled}
+            >
+                <span>{children}</span>
+
+                <div className="flex items-center gap-2">
+                    {shortcut && (
+                        <span className={` ${disabled ? 'opacity-40' : 'text-gray'} ml-2 text-xs select-none`}>
+                            {shortcut}
+                        </span>
+                    )}
+                    {submenu && (
+                        <span className="ml-2 text-gray text-xs select-none">
+                            <ChevronRight size={16} />
+                        </span>
+                    )}
+                </div>
+            </button>
+
+            {submenu && isSubmenuOpen && (
+                <div className="absolute top-0 left-full min-w-40 w-full bg-card border border-border-light rounded shadow-md py-1 text-sm z-50">
+                    {submenu}
+                </div>
             )}
-        </button>
+        </div>
     );
 };
