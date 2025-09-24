da72f65d8a0253bc264f8d9546f6c485b13a694a
Attato <rde8519@mail.ru>
2025-09-24 21:16:55 +0300

    useCanvasSelection init

---
+++ app/canvas/hooks/useCanvasSelection.ts
@@ -0,0 +1,26 @@
+'use client';
+
+import { useState, useCallback } from 'react';
+import { Position } from '@/canvas/canvas.types';
+import { useCanvasStore } from '@/canvas/store/сanvasStore';
+import { getNodes } from '@/canvas/utils/getNodes';
+import { getItemsInSelectionArea } from '@/canvas/utils/getItemsInSelectionArea';
+
+export function useCanvasSelection() {
+    const { setSelectedItemIds } = useCanvasStore();
+
+    const [selectionStart, setSelectionStart] = useState<Position | null>(null);
+    const [selectionEnd, setSelectionEnd] = useState<Position | null>(null);
+
+    const handleSelectionArea = useCallback(
+        (start: Position, end: Position) => {
+            const items = useCanvasStore.getState().items;
+            const nodes = getNodes(items);
+            const selected = getItemsInSelectionArea(nodes, start, end);
+            setSelectedItemIds(selected);
+        },
+        [setSelectedItemIds],
+    );
+
+    return { selectionStart, selectionEnd, setSelectionStart, setSelectionEnd, handleSelectionArea };
+}
