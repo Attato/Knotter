222d698d345c85475b0866d5f1f367c55e82a032
Attato <rde8519@mail.ru>
2025-09-25 21:41:41 +0300

    NODE_SHAPE_TYPES add

--- app/canvas/constants.ts
+++ app/canvas/constants.ts
@@ -1,3 +1,5 @@
+import { NodeShapeType } from '@/canvas/canvas.types';
+
 export const MIN_ZOOM = 0.1;
 export const MAX_ZOOM = 10;
 export const INITIAL_ZOOM = 1;
@@ -9,3 +11,13 @@ export const NODE_MOVE_MIN_STEP = 1;
 export const NODE_MOVE_MAX_STEP = 50;
 
 export const MAX_CANVAS_ITEMS = 250;
+
+export const NODE_SHAPE_TYPES: NodeShapeType[] = [
+    'octagon',
+    'circle',
+    'squircle',
+    'hexagon',
+    'triangle',
+    'diamond',
+    'point',
+];
