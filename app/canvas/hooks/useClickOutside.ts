c91b3fff8a5867ebc87eaddbcb7b09215f230d94
Attato <rde8519@mail.ru>
2025-09-30 20:58:37 +0300

    useClickOutside init

---
+++ app/canvas/hooks/useClickOutside.ts
@@ -0,0 +1,14 @@
+import { useEffect, RefObject } from 'react';
+
+export function useClickOutside<T extends HTMLElement>(ref: RefObject<T | null>, onClose: () => void) {
+    useEffect(() => {
+        function handleClick(event: MouseEvent) {
+            if (ref.current && !ref.current.contains(event.target as Node)) {
+                onClose();
+            }
+        }
+
+        document.addEventListener('mousedown', handleClick);
+        return () => document.removeEventListener('mousedown', handleClick);
+    }, [ref, onClose]);
+}
