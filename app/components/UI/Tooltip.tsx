1c6fe28af828e23d2617298b26e7f3561272e75b
Attato <rde8519@mail.ru>
2025-10-06 19:47:18 +0300

    add select-none

--- app/components/UI/Tooltip.tsx
+++ app/components/UI/Tooltip.tsx
@@ -9,7 +9,7 @@ export const Tooltip = ({ label, children }: TooltipProps) => {
     return (
         <div className="relative group">
             {children}
-            <span className="absolute top-10 right-1/2 translate-x-1/2 px-2 py-1 text-xs text-foreground bg-background-alt border border-border rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap pointer-events-none">
+            <span className="absolute top-10 right-1/2 translate-x-1/2 px-2 py-1 text-xs text-foreground bg-background-alt border border-border rounded opacity-0 group-hover:opacity-100 transition-opacity whitespace-nowrap pointer-events-none select-none">
                 {label}
             </span>
         </div>
