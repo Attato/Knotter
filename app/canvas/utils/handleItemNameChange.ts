c77b66b760ed40940e8d68437f40aba99d432ae9
Attato <rde8519@mail.ru>
2025-10-06 19:57:41 +0300

    update handleItemNameChange to use setState callback

--- app/canvas/utils/handleItemNameChange.ts
+++ app/canvas/utils/handleItemNameChange.ts
@@ -2,8 +2,7 @@ import { CanvasItem } from '@/canvas/canvas.types';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
 
 export const handleItemNameChange = (item: CanvasItem, newName: string) => {
-    const { items, setItems } = useCanvasStore.getState();
-
-    const updatedItems = items.map((i) => (i.id === item.id ? { ...i, name: newName } : i));
-    setItems(updatedItems);
+    useCanvasStore.setState((state) => ({
+        items: state.items.map((i) => (i.id === item.id && i.name !== newName ? { ...i, name: newName } : i)),
+    }));
 };
