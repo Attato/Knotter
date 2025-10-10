fd351f0e85be988fb19cfc664e79840fe885bc91
Attato <rde8519@mail.ru>
2025-10-10 18:45:00 +0300

    add dynamic dropdowns to inspector

--- app/canvas/components/Inspector.tsx
+++ app/canvas/components/Inspector.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { memo, useMemo } from 'react';
+import { memo, useMemo, useState, useCallback } from 'react';
 
 import { useInspector } from '@/canvas/hooks/useInspector';
 
@@ -10,6 +10,10 @@ import { Dropdown } from '@/components/UI/Dropdown';
 
 import { ShapeButtons } from '@/canvas/components/ShapeButtons';
 import { PositionInputs } from '@/canvas/components/PositionInputs';
+
+import { v4 as uuidv4 } from 'uuid';
+
+import { Plus } from 'lucide-react';
 
 export const Inspector = memo(function Inspector() {
     const { selectedItem, currentItem, handleChangeName, handleChangeDescription, handleChangeNodeShapeType, handleMove } =
@@ -32,6 +36,21 @@ export const Inspector = memo(function Inspector() {
         return <PositionInputs positionX={positionX} positionY={positionY} onMove={handleMove} />;
     }, [selectedItem, isEdge, positionX, positionY, handleMove]);
 
+    const [dropdowns, setDropdowns] = useState<{ id: number | string; title: string }[]>([
+        { id: 1, title: 'Форма' },
+        { id: 2, title: 'Трансформация' },
+    ]);
+
+    const addDropdown = useCallback(() => {
+        setDropdowns((prev) => [
+            ...prev,
+            {
+                id: uuidv4(),
+                title: `Выпадающий список (${prev.length + 1})`,
+            },
+        ]);
+    }, []);
+
     if (!selectedItem) {
         return (
             <div className="flex justify-center items-center h-full text-gray text-sm text-center">
@@ -41,18 +60,35 @@ export const Inspector = memo(function Inspector() {
     }
 
     return (
-        <div className="flex flex-col h-full overflow-y-auto m-2 gap-2">
+        <div className="flex flex-col h-full overflow-y-auto m-1 gap-1">
             <Input value={selectedItem.name} onChange={handleChangeName} placeholder="Название" />
-
             <Textarea value={selectedItem.description} onChange={handleChangeDescription} placeholder="Описание" />
 
-            <Dropdown title="Форма" disabled={isEdge}>
-                {shapeButtons}
-            </Dropdown>
+            {dropdowns
+                .filter((dd) => typeof dd.id === 'number')
+                .map((dd) => (
+                    <Dropdown key={dd.id} title={dd.title} disabled={isEdge}>
+                        {dd.id === 1 ? shapeButtons : dd.id === 2 ? positionInputs : null}
+                    </Dropdown>
+                ))}
 
-            <Dropdown title="Трансформация" disabled={isEdge}>
-                {positionInputs}
-            </Dropdown>
+            <hr className="border-b-0 border-border" />
+
+            {dropdowns
+                .filter((dd) => typeof dd.id === 'string')
+                .map((dd) => (
+                    <Dropdown key={dd.id} title={dd.title}>
+                        <></>
+                    </Dropdown>
+                ))}
+
+            <button
+                onClick={addDropdown}
+                className="flex justify-start gap-2 items-center px-3 py-2 w-full text-sm cursor-pointer bg-card hover:bg-ui rounded-md"
+            >
+                <Plus size={16} />
+                Добавить выпадающий список
+            </button>
         </div>
     );
 });
