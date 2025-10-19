fdb38e502e8d61f48079ee66d2563fde108328ec
Attato <rde8519@mail.ru>
2025-10-19 21:13:43 +0300

    getSelectionEventHandler init

---
+++ app/canvas/utils/eventHandlers/getSelectionEventHandler.ts
@@ -0,0 +1,41 @@
+import { Position } from '@/canvas/canvas.types';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+export function getSelectionEventHandler(
+    setSelectionStart: (value: Position | null) => void,
+    setSelectionEnd: (value: Position | null) => void,
+    onSelectionComplete?: (start: Position, end: Position) => void,
+) {
+    let start: Position | null = null;
+
+    const handleMouseDown = (e: MouseEvent) => {
+        if (e.button !== 0) return;
+
+        const mousePos = useCanvasStore.getState().mousePosition;
+        start = mousePos;
+
+        setSelectionStart(mousePos);
+        setSelectionEnd(mousePos);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+        if (!start || (e.buttons & 1) !== 1) return;
+
+        const mousePos = useCanvasStore.getState().mousePosition;
+
+        setSelectionEnd(mousePos);
+    };
+
+    const handleMouseUp = (e: MouseEvent) => {
+        if (!start || e.button !== 0) return;
+
+        const mousePos = useCanvasStore.getState().mousePosition;
+
+        onSelectionComplete?.(start, mousePos);
+        setSelectionStart(null);
+        setSelectionEnd(null);
+        start = null;
+    };
+
+    return { handleMouseDown, handleMouseMove, handleMouseUp };
+}
