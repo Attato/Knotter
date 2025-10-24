d3b78a7a2a8b4917bfd0726cc073893c15e15d04
Attato <rde8519@mail.ru>
2025-10-24 20:51:29 +0300

    rename variables to parameters inside ParametersItem component

--- app/canvas/components/Parameters/ParametersItem.tsx
+++ app/canvas/components/Parameters/ParametersItem.tsx
@@ -11,28 +11,28 @@ import { useParametersItem } from '@/canvas/hooks/Parameters/useParametersItem';
 import { X } from 'lucide-react';
 
 interface ParametersItemProps {
-    variableId: string;
-    onRemoveVariable: (id: string) => void;
+    parameterId: string;
+    onRemoveParameter: (id: string) => void;
 }
 
-export const ParametersItem = memo(function ParametersItem({ variableId, onRemoveVariable }: ParametersItemProps) {
+export const ParametersItem = memo(function ParametersItem({ parameterId, onRemoveParameter }: ParametersItemProps) {
     const {
-        variable,
-        updateVariable,
-        updateVariableName,
+        parameter,
+        updateParameter,
+        updateParameterName,
         updateEnumOption,
         addEnumOption,
         removeEnumOption,
         handleNumberInput,
         getDisplayValue,
-    } = useParametersItem(variableId);
+    } = useParametersItem(parameterId);
 
     return (
         <div className="flex flex-col justify-center gap-2 px-3 py-1 min-h-[44px] text-sm bg-card rounded-md">
             <div className="flex items-center gap-2">
-                <EditableName name={variable.name} onChange={updateVariableName} className="w-full" />
+                <EditableName name={parameter.name} onChange={updateParameterName} className="w-full" />
 
-                {variable.type === 'number' ? (
+                {parameter.type === 'number' ? (
                     <Input
                         value={getDisplayValue()}
                         onChange={handleNumberInput}
@@ -41,20 +41,22 @@ export const ParametersItem = memo(function ParametersItem({ variableId, onRemov
                         type="text"
                         inputMode="decimal"
                     />
-                ) : variable.type === 'string' ? (
-                    <Input value={String(variable.value)} onChange={updateVariable} className="bg-ui w-48" max={16} />
+                ) : parameter.type === 'string' ? (
+                    <Input value={String(parameter.value)} onChange={updateParameter} className="bg-ui w-48" max={16} />
                 ) : null}
 
-                {variable.type === 'boolean' && <Checkbox checked={variable.value as boolean} onChange={updateVariable} />}
+                {parameter.type === 'boolean' && (
+                    <Checkbox checked={parameter.value as boolean} onChange={updateParameter} />
+                )}
 
-                <button onClick={() => onRemoveVariable(variable.id)} className="ml-auto text-gray cursor-pointer">
+                <button onClick={() => onRemoveParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
                     <X size={16} />
                 </button>
             </div>
 
-            {variable.type === 'enum' && (
+            {parameter.type === 'enum' && (
                 <div className="flex flex-col gap-1 ml-32">
-                    {(variable.value as string[]).map((opt, idx) => (
+                    {(parameter.value as string[]).map((opt, idx) => (
                         <div key={idx} className="flex gap-1 items-center">
                             <p className="tabular-nums min-w-[4ch]">{idx + 1}.</p>
 
@@ -67,8 +69,8 @@ export const ParametersItem = memo(function ParametersItem({ variableId, onRemov
 
                             <button
                                 onClick={() => removeEnumOption(idx)}
-                                className={`${(variable.value as string[]).length === 1 ? 'text-gray/50' : 'text-gray'} cursor-pointer`}
-                                disabled={(variable.value as string[]).length === 1}
+                                className={`${(parameter.value as string[]).length === 1 ? 'text-gray/50' : 'text-gray'} cursor-pointer`}
+                                disabled={(parameter.value as string[]).length === 1}
                             >
                                 <X size={16} />
                             </button>
