a9636c5b14ad93f94ea3bef223e06f5e7ac74335
Attato <rde8519@mail.ru>
2025-11-06 02:47:22 +0300

    hide the CanvasStatusBar during full-screen mode

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -10,9 +10,12 @@ import { useCanvasSelection } from '@/canvas/hooks/useCanvasSelection';
 import { useCanvasInteraction } from '@/canvas/hooks/useCanvasInteraction';
 import { useCanvasRenderer } from '@/canvas/hooks/useCanvasRenderer';
 import { useContextMenu } from '@/hooks/useContextMenu';
+import { useCanvasStore } from './store/canvasStore';
 
 export default function Canvas() {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    const isFullScreen = useCanvasStore((state) => state.isFullScreen);
 
     const { selectionStart, selectionEnd, setSelectionStart, setSelectionEnd, selectItemsInArea } = useCanvasSelection();
 
@@ -30,8 +33,7 @@ export default function Canvas() {
 
     return (
         <div className="flex h-screen relative" onClick={closeMenu}>
-            <CanvasStatusBar canvasRef={canvasRef} />
-
+            {!isFullScreen && <CanvasStatusBar canvasRef={canvasRef} />}
             <CanvasControls />
 
             <CanvasContextMenu isOpen={isOpen} position={position} closeMenu={closeMenu} canvasRef={canvasRef} />
