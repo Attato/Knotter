296677d93234ef4f703f286bf438cc997f6cd929
Attato <rde8519@mail.ru>
2025-11-07 14:35:56 +0300

    add className support

--- app/components/UI/Checkbox.tsx
+++ app/components/UI/Checkbox.tsx
@@ -6,9 +6,10 @@ import { Check } from 'lucide-react';
 interface CheckboxProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'type'> {
     checked: boolean;
     onChange: (checked: boolean) => void;
+    className?: string;
 }
 
-export const Checkbox = memo(function Checkbox({ checked, onChange, className = '', ...props }: CheckboxProps) {
+export const Checkbox = memo(function Checkbox({ checked, onChange, className = 'bg-ui', ...props }: CheckboxProps) {
     const handleChange = useCallback(
         (e: React.ChangeEvent<HTMLInputElement>) => {
             onChange(e.target.checked);
@@ -18,15 +19,11 @@ export const Checkbox = memo(function Checkbox({ checked, onChange, className = 
 
     return (
         <label className="relative flex w-fit cursor-pointer">
-            <input
-                type="checkbox"
-                checked={checked}
-                onChange={handleChange}
-                className={`sr-only peer ${className}`}
-                {...props}
-            />
+            <input type="checkbox" checked={checked} onChange={handleChange} className="sr-only peer" {...props} />
 
-            <div className="w-5 h-5 bg-ui rounded-sm peer-focus:ring-2 peer-focus:ring-bg-accent peer-checked:bg-bg-accent peer-checked:border-bg-accent flex items-center justify-center">
+            <div
+                className={`w-5 h-5 ${className} rounded-sm peer-focus:ring-2 peer-focus:ring-bg-accent peer-checked:bg-bg-accent peer-checked:border-bg-accent flex items-center justify-center`}
+            >
                 {checked && <Check size={16} className="text-white" />}
             </div>
         </label>
