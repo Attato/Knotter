75649c87abd52753a6b1b71d3aa41f057b344633
Attato <rde8519@mail.ru>
2025-11-07 03:06:40 +0300

    use parameters utils

--- app/canvas/hooks/Parameters/useParameters.ts
+++ app/canvas/hooks/Parameters/useParameters.ts
@@ -2,44 +2,13 @@ import { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
 import { useParametersStore } from '@/canvas/store/parametersStore';
-
-import { ParameterType, ParameterValue, Parameter } from '@/canvas/canvas.types';
-
-const PARAMETER_TYPES: { label: string; value: ParameterType }[] = [
-    { label: 'Число', value: 'number' },
-    { label: 'Строка', value: 'string' },
-    { label: 'Флаг', value: 'boolean' },
-    { label: 'Список', value: 'enum' },
-    { label: 'Массив', value: 'array' },
-];
+import { ParameterType, Parameter } from '@/canvas/canvas.types';
+import { createInitialParameterValue } from '@/canvas/utils/items/parameters.utils';
 
 export const useParameters = () => {
     const { parameters, setParameters } = useParametersStore();
     const [name, setName] = useState('');
     const [type, setType] = useState<ParameterType>('number');
-
-    const createInitialValue = (type: ParameterType): ParameterValue => {
-        const initialEnumValue = {
-            options: {
-                id: uuid(),
-                values: [''],
-            },
-            selectedId: null,
-        };
-
-        switch (type) {
-            case 'number':
-                return 0;
-            case 'string':
-                return '';
-            case 'boolean':
-                return false;
-            case 'enum':
-                return initialEnumValue;
-            case 'array':
-                return [];
-        }
-    };
 
     const addParameter = () => {
         if (!name) return;
@@ -48,7 +17,7 @@ export const useParameters = () => {
             id: uuid(),
             name,
             type,
-            value: createInitialValue(type),
+            value: createInitialParameterValue(type),
         };
 
         setParameters([...parameters, newParameter]);
@@ -63,7 +32,6 @@ export const useParameters = () => {
         name,
         type,
         parameters,
-        parameterTypes: PARAMETER_TYPES,
 
         setName,
         setType,
