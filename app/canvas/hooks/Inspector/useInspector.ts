d59975c5674e4db3acfc801876bc9f657f493eff
Attato <rde8519@mail.ru>
2025-11-11 16:30:07 +0300

    fix handleItemNameChange

--- app/canvas/hooks/Inspector/useInspector.ts
+++ app/canvas/hooks/Inspector/useInspector.ts
@@ -2,21 +2,25 @@
 
 import { useCallback } from 'react';
 import { useCanvasStore } from '@/canvas/store/canvasStore';
-
-import { handleItemNameChange } from '@/canvas/utils/items/handleItemNameChange';
+import { CanvasItem } from '@/canvas/canvas.types';
 
 export function useInspector() {
     const items = useCanvasStore((state) => state.items);
     const setItems = useCanvasStore((state) => state.setItems);
     const selectedItem = useCanvasStore((state) => state.selectedItem);
 
+    const handleItemNameChange = (items: CanvasItem[], item: CanvasItem, newName: string): CanvasItem[] => {
+        return items.map((i) => (i.id === item.id && i.name !== newName ? { ...i, name: newName } : i));
+    };
+
     const handleChangeName = useCallback(
         (newName: string) => {
             if (!selectedItem) return;
 
-            handleItemNameChange(selectedItem, newName);
+            const updatedItems = handleItemNameChange(items, selectedItem, newName);
+            setItems(updatedItems);
         },
-        [selectedItem],
+        [selectedItem, items, setItems],
     );
 
     const handleChangeDescription = useCallback(
@@ -26,7 +30,6 @@ export function useInspector() {
             const updatedItems = items.map((item) =>
                 item.id === selectedItem.id ? { ...item, description: newDesc } : item,
             );
-
             setItems(updatedItems);
         },
         [selectedItem, items, setItems],
