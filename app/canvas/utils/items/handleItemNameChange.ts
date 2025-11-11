41369ddb57e34f32f673328abdd6ecb1d2f50e5a
Attato <rde8519@mail.ru>
2025-11-11 16:29:44 +0300

    delete handleItemNameChange

--- app/canvas/utils/items/handleItemNameChange.ts
+++
@@ -1,8 +0,0 @@
-import { CanvasItem } from '@/canvas/canvas.types';
-import { useCanvasStore } from '@/canvas/store/canvasStore';
-
-export const handleItemNameChange = (item: CanvasItem, newName: string) => {
-    useCanvasStore.setState((state) => ({
-        items: state.items.map((i) => (i.id === item.id && i.name !== newName ? { ...i, name: newName } : i)),
-    }));
-};
