6bb2d1655d85d9f6fe18d41469d66830bd447baf
Attato <rde8519@mail.ru>
2025-11-13 23:30:08 +0300

    redesign PropertyParameterItem component layout

--- app/canvas/components/Property/PropertyParameterItem.tsx
+++ app/canvas/components/Property/PropertyParameterItem.tsx
@@ -35,61 +35,29 @@ export const PropertyParameterItem = memo(function PropertyParameterItem({
 
     const Icon = getDynamicIcon(parameter.type);
 
-    const renderParameterInput = () => {
-        if (parameterType.isNumber) {
-            return (
-                <Input
-                    value={String(value ?? 0)}
-                    onChange={handleNumber}
-                    className="w-full bg-ui"
-                    type="text"
-                    inputMode="decimal"
-                />
-            );
-        }
+    if (parameterType.isArray) {
+        if (!arrayData) return null;
 
-        if (parameterType.isString) {
-            return (
-                <Input
-                    value={String(value ?? '')}
-                    onChange={handleString}
-                    className="w-full bg-ui"
-                    placeholder="Введите текст..."
-                />
-            );
-        }
+        return (
+            <div className="flex flex-col gap-2 bg-card rounded-md py-2 px-3">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <Icon size={16} />
+                        <p className="text-sm font-medium text-foreground truncate">{name}</p>
+                    </div>
 
-        if (parameterType.isBoolean) {
-            return (
-                <div className="flex items-center h-[36px]">
-                    <Checkbox checked={Boolean(value)} onChange={handleBoolean} />
+                    <button
+                        onClick={handleRemoveParameter}
+                        className="text-gray cursor-pointer w-8 h-8 flex items-center justify-center"
+                    >
+                        <X size={16} />
+                    </button>
                 </div>
-            );
-        }
 
-        if (parameterType.isEnum) {
-            if (!enumData) return null;
-
-            return (
-                <Select
-                    value={enumData.selectedDisplay}
-                    onChange={handleEnum}
-                    options={enumData.options}
-                    className="w-full border"
-                />
-            );
-        }
-
-        if (parameterType.isArray) {
-            if (!arrayData) return null;
-
-            return (
-                <div className="flex flex-col gap-2 w-full">
+                <div className="flex flex-col gap-1 w-full pl-6">
                     {arrayData.length === 0 && <p className="p-2 text-gray text-sm">Массив пуст</p>}
 
                     {arrayData.map((item) => {
-                        const Icon = getDynamicIcon(item.type);
-
                         if (item.type === 'enum' && item.value && typeof item.value === 'object') {
                             const enumValue = item.value as Enum;
                             if (!enumValue.options) return null;
@@ -113,28 +81,24 @@ export const PropertyParameterItem = memo(function PropertyParameterItem({
                             };
 
                             return (
-                                <div key={item.id} className="flex items-center gap-2 bg-border rounded-md px-3 py-1">
-                                    <div className="flex items-center gap-2 w-full">
-                                        <Icon size={16} />
-                                        <p className="text-sm font-medium text-foreground truncate">{item.name}</p>
-                                    </div>
+                                <div key={item.id} className="flex items-center justify-end gap-2">
+                                    <p className="text-sm font-medium text-foreground text-end truncate w-full">
+                                        {item.name}
+                                    </p>
 
                                     <Select
                                         value={currentValue}
                                         onChange={handleEnumChange}
                                         options={options}
-                                        className="bg-ui border border-ui-hover"
+                                        className="bg-border border border-ui"
                                     />
                                 </div>
                             );
                         }
 
                         return (
-                            <div key={item.id} className="flex items-center gap-2 bg-border rounded-md px-3 py-1">
-                                <div className="flex items-center gap-2 w-full">
-                                    <Icon size={16} />
-                                    <p className="text-sm font-medium text-foreground truncate">{item.name}</p>
-                                </div>
+                            <div key={item.id} className="flex items-center gap-2">
+                                <p className="text-sm font-medium text-foreground text-end truncate w-full">{item.name}</p>
 
                                 {item.type === 'number' && (
                                     <Input
@@ -143,7 +107,7 @@ export const PropertyParameterItem = memo(function PropertyParameterItem({
                                             const num = parseFloat(val);
                                             handleArrayItem(item.id, isNaN(num) ? 0 : num, 'number');
                                         }}
-                                        className="w-full bg-ui border border-ui-hover h-[36px]"
+                                        className="w-full bg-border border border-ui"
                                         inputMode="decimal"
                                     />
                                 )}
@@ -152,17 +116,17 @@ export const PropertyParameterItem = memo(function PropertyParameterItem({
                                     <Input
                                         value={String(item.value ?? '')}
                                         onChange={(val) => handleArrayItem(item.id, val, 'string')}
-                                        className="w-full bg-ui border border-ui-hover h-[36px]"
+                                        className="w-full bg-border border border-ui"
                                         placeholder="Введите текст..."
                                     />
                                 )}
 
                                 {item.type === 'boolean' && (
-                                    <div className="flex items-center w-full h-[36px]">
+                                    <div className="flex items-center w-full">
                                         <Checkbox
                                             checked={Boolean(item.value)}
                                             onChange={(checked) => handleArrayItem(item.id, checked, 'boolean')}
-                                            className="bg-ui border border-ui-hover"
+                                            className="bg-border border border-ui"
                                         />
                                     </div>
                                 )}
@@ -170,30 +134,74 @@ export const PropertyParameterItem = memo(function PropertyParameterItem({
                         );
                     })}
                 </div>
+            </div>
+        );
+    }
+
+    const renderInput = () => {
+        if (parameterType.isNumber) {
+            return (
+                <Input
+                    value={String(value ?? 0)}
+                    onChange={handleNumber}
+                    className="w-full bg-border"
+                    type="text"
+                    inputMode="decimal"
+                />
             );
         }
 
-        return null;
+        if (parameterType.isString) {
+            return (
+                <Input
+                    value={String(value ?? '')}
+                    onChange={handleString}
+                    className="w-full bg-border"
+                    placeholder="Введите текст..."
+                />
+            );
+        }
+
+        if (parameterType.isBoolean) {
+            return (
+                <div className="flex items-center">
+                    <Checkbox checked={Boolean(value)} onChange={handleBoolean} />
+                </div>
+            );
+        }
+
+        if (parameterType.isEnum) {
+            if (!enumData) return null;
+
+            return (
+                <Select
+                    value={enumData.selectedDisplay}
+                    onChange={handleEnum}
+                    options={enumData.options}
+                    className="w-full border"
+                />
+            );
+        }
     };
 
     return (
-        <div className="flex flex-col gap-2 bg-card rounded-md py-2 px-3">
-            <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
+        <div className="bg-card px-3 py-1 rounded-md">
+            <div className="flex items-center justify-between gap-2">
+                <div className="flex items-center gap-2 w-full">
                     <Icon size={16} />
                     <p className="text-sm font-medium text-foreground truncate">{name}</p>
                 </div>
 
+                {renderInput()}
+
                 <button
                     onClick={handleRemoveParameter}
-                    className="text-gray cursor-pointer w-[36px] h-[36px] flex items-center justify-center"
+                    className="text-gray cursor-pointer w-8 h-8 flex items-center justify-center"
                     title="Удалить параметр"
                 >
                     <X size={16} />
                 </button>
             </div>
-
-            {renderParameterInput()}
         </div>
     );
 });
