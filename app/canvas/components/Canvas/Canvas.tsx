0548950653979ee7b8fa14ebaa5ac711930d9bce
Attato <rde8519@mail.ru>
2025-11-15 02:15:32 +0300

    Canvas init

---
+++ app/canvas/components/Canvas/Canvas.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useRef } from 'react';
+
+import { CanvasContextMenu } from '@/canvas/components/CanvasContextMenu/CanvasContextMenu';
+import { CanvasControls } from '@/canvas/components/CanvasControls/CanvasControls';
+import { CanvasStatusBar } from '@/canvas/components/CanvasStatusBar/CanvasStatusBar';
+
+import { useCanvasSelection } from '@/canvas/hooks/useCanvasSelection';
+import { useCanvasInteraction } from '@/canvas/hooks/useCanvasInteraction';
+import { useCanvasRenderer } from '@/canvas/hooks/useCanvasRenderer';
+import { useContextMenu } from '@/hooks/useContextMenu';
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+export default function Canvas() {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    const isFullScreen = useCanvasStore((state) => state.isFullScreen);
+
+    const { selectionStart, selectionEnd, setSelectionStart, setSelectionEnd, selectItemsInArea } = useCanvasSelection();
+
+    useCanvasInteraction({
+        canvasRef,
+        selectionStart,
+        setSelectionStart,
+        setSelectionEnd,
+        selectItemsInArea,
+    });
+
+    useCanvasRenderer({ canvasRef, selectionStart, selectionEnd });
+
+    const { isOpen, position, handleContextMenu, closeMenu } = useContextMenu();
+
+    return (
+        <div className="flex h-screen relative" onClick={closeMenu}>
+            {!isFullScreen && <CanvasStatusBar canvasRef={canvasRef} />}
+
+            <CanvasControls />
+
+            <CanvasContextMenu isOpen={isOpen} position={position} closeMenu={closeMenu} canvasRef={canvasRef} />
+
+            <canvas ref={canvasRef} className="absolute w-full h-full" onContextMenu={handleContextMenu} />
+        </div>
+    );
+}
