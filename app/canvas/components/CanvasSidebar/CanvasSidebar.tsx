27c5534ed366789123629aaa0856579247ae40c6
Attato <rde8519@mail.ru>
2025-11-16 20:52:38 +0300

    split CanvasSidebar into SidebarTabs and SidebarPanels

--- app/canvas/components/CanvasSidebar/CanvasSidebar.tsx
+++ app/canvas/components/CanvasSidebar/CanvasSidebar.tsx
@@ -1,14 +1,24 @@
 'use client';
 
-import { memo } from 'react';
+import { memo, type ReactNode } from 'react';
 
 import { Sidebar } from '@/components/UI/Sidebar';
-import { Tabs, Tab } from '@/components/UI/Tabs';
+
+import { SidebarTabs } from '@/canvas/components/CanvasSidebar/SidebarTabs';
+import { SidebarPanels } from '@/canvas/components/CanvasSidebar/SidebarPanels';
 
 import { Hierarchy } from '@/canvas/components/Hierarchy/Hierarchy';
 import { Inspector } from '@/canvas/components/Inspector/Inspector';
 import { Parameters } from '@/canvas/components/Parameters/Parameters';
-import { ListTree, Info, Variable } from 'lucide-react';
+
+import { ListTree, Info, Variable, type LucideIcon } from 'lucide-react';
+
+export interface Tab {
+    id: string;
+    label: string;
+    content?: ReactNode;
+    icon?: LucideIcon;
+}
 
 const tabs: Tab[] = [
     {
@@ -33,10 +43,12 @@ const tabs: Tab[] = [
 
 export const CanvasSidebar = memo(function CanvasSidebar() {
     return (
-        <Sidebar>
-            <div className="flex flex-col h-full relative">
-                <Tabs tabs={tabs} defaultTab="hierarchy" className="flex-1" />
-            </div>
-        </Sidebar>
+        <div className="flex h-full">
+            <Sidebar tabs={tabs}>
+                <SidebarPanels tabs={tabs} />
+            </Sidebar>
+
+            <SidebarTabs tabs={tabs.map(({ id, label, icon }) => ({ id, label, icon }))} />
+        </div>
     );
 });
