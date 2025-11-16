d3328bf9483b19e3dc86924f29d910eae2692547
Attato <rde8519@mail.ru>
2025-11-16 04:32:14 +0300

    extract base parameter into separate component

--- app/canvas/components/Parameters/ParametersItem.tsx
+++ app/canvas/components/Parameters/ParametersItem.tsx
@@ -2,24 +2,11 @@
 
 import { memo } from 'react';
 
-import { Input } from '@/components/UI/Input';
-import { EditableName } from '@/components/UI/EditableName';
-import { Checkbox } from '@/components/UI/Checkbox';
-
 import { EnumContent } from '@/canvas/components/Parameters/EnumContent';
 import { ArrayContent } from '@/canvas/components/Parameters/ArrayContent';
+import { BaseParameterContent } from '@/canvas/components/Parameters/BaseParameterContent';
 
-import {
-    useParametersItem,
-    isNumberValue,
-    isStringValue,
-    isBooleanValue,
-    isEnumValue,
-    isArrayValue,
-} from '@/canvas/hooks/Parameters/useParametersItem';
-
-import { getDynamicIcon } from '@/canvas/utils/items/getDynamicIcon';
-import { X } from 'lucide-react';
+import { useParametersItem, isEnumValue, isArrayValue } from '@/canvas/hooks/Parameters/useParametersItem';
 
 interface ParametersItemProps {
     parameterId: string;
@@ -43,60 +30,6 @@ export const ParametersItem = memo(function ParametersItem({ parameterId, onRemo
     } = useParametersItem(parameterId);
 
     const parameterValue = parameter.value;
-
-    const Icon = getDynamicIcon(parameterType);
-
-    const renderBaseParameter = () => (
-        <div
-            className="flex flex-col justify-center gap-2 px-3 py-1 text-sm bg-card rounded-md"
-            draggable
-            onDragStart={(e) => {
-                e.dataTransfer.setData('application/parameter-id', parameter.id);
-                e.dataTransfer.setData('application/parameter-type', parameterType);
-            }}
-        >
-            <div className="flex items-center gap-1">
-                <Icon size={16} className="min-w-4" />
-
-                <EditableName name={parameter.name} onChange={updateParameterName} className="w-full" />
-
-                {isNumberValue(parameterValue) && (
-                    <Input
-                        value={getDisplayValue()}
-                        onChange={handleNumberInput}
-                        className="bg-border border border-ui"
-                        max={16}
-                        type="text"
-                        inputMode="decimal"
-                    />
-                )}
-
-                {isStringValue(parameterValue) && (
-                    <Input
-                        value={parameterValue}
-                        onChange={(val) => updateParameter(val)}
-                        className="bg-border border border-ui"
-                        max={16}
-                        placeholder="Введите текст..."
-                    />
-                )}
-
-                {isBooleanValue(parameterValue) && (
-                    <div className="w-full">
-                        <Checkbox
-                            checked={parameterValue}
-                            onChange={(checked) => updateParameter(checked)}
-                            className="bg-border border border-ui"
-                        />
-                    </div>
-                )}
-
-                <button onClick={() => onRemoveParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
-                    <X size={16} />
-                </button>
-            </div>
-        </div>
-    );
 
     if (isEnumValue(parameterValue)) {
         return (
@@ -129,5 +62,17 @@ export const ParametersItem = memo(function ParametersItem({ parameterId, onRemo
         );
     }
 
-    return renderBaseParameter();
+    return (
+        <BaseParameterContent
+            parameterId={parameter.id}
+            parameterName={parameter.name}
+            parameterType={parameterType}
+            parameterValue={parameterValue}
+            updateParameterName={updateParameterName}
+            updateParameter={updateParameter}
+            handleNumberInput={handleNumberInput}
+            getDisplayValue={getDisplayValue}
+            onRemove={() => onRemoveParameter(parameter.id)}
+        />
+    );
 });
