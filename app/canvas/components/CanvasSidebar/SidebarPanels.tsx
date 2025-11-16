6b45a7a089d233690265a3ce1608b61dc09ec59e
Attato <rde8519@mail.ru>
2025-11-16 20:51:43 +0300

    SidebarPanels init

---
+++ app/canvas/components/CanvasSidebar/SidebarPanels.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+import type { Tab } from '@/canvas/components/CanvasSidebar/CanvasSidebar';
+
+interface SidebarPanelsProps {
+    tabs: Tab[];
+}
+
+export function SidebarPanels({ tabs }: SidebarPanelsProps) {
+    const activeTab = useCanvasStore((s) => s.activeTab);
+
+    const panel = tabs.find((t) => t.id === activeTab)?.content;
+
+    return <div className="flex-1 overflow-auto bg-background-alt border-r border-border">{panel}</div>;
+}
