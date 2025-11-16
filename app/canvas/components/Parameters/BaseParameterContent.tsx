86c802ab7a9650b6e716b1c172ac87db0af7526f
Attato <rde8519@mail.ru>
2025-11-16 04:31:42 +0300

    BaseParameterContent init

---
+++ app/canvas/components/Parameters/BaseParameterContent.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import { memo } from 'react';
+
+import { Input } from '@/components/UI/Input';
+import { EditableName } from '@/components/UI/EditableName';
+import { Checkbox } from '@/components/UI/Checkbox';
+
+import { getDynamicIcon } from '@/canvas/utils/items/getDynamicIcon';
+
+import { isNumberValue, isStringValue, isBooleanValue } from '@/canvas/hooks/Parameters/useParametersItem';
+
+import { X } from 'lucide-react';
+
+type BaseParameterValue = number | string | boolean;
+
+interface BaseParameterContentProps {
+    parameterId: string;
+    parameterName: string;
+    parameterType: string;
+    parameterValue: BaseParameterValue;
+
+    updateParameterName: (name: string) => void;
+    updateParameter: (value: BaseParameterValue) => void;
+    handleNumberInput: (value: string) => void;
+    getDisplayValue: () => string;
+
+    onRemove: () => void;
+}
+
+export const BaseParameterContent = memo(function BaseParameterContent({
+    parameterId,
+    parameterName,
+    parameterType,
+    parameterValue,
+
+    updateParameterName,
+    updateParameter,
+    handleNumberInput,
+    getDisplayValue,
+
+    onRemove,
+}: BaseParameterContentProps) {
+    const Icon = getDynamicIcon(parameterType);
+
+    return (
+        <div
+            className="flex flex-col justify-center gap-2 px-3 py-1 text-sm bg-card rounded-md"
+            draggable
+            onDragStart={(e) => {
+                e.dataTransfer.setData('application/parameter-id', parameterId);
+                e.dataTransfer.setData('application/parameter-type', parameterType);
+            }}
+        >
+            <div className="flex items-center gap-1">
+                <Icon size={16} className="min-w-4" />
+
+                <EditableName name={parameterName} onChange={updateParameterName} className="w-full" />
+
+                {isNumberValue(parameterValue) && (
+                    <Input
+                        value={getDisplayValue()}
+                        onChange={handleNumberInput}
+                        className="bg-border border border-ui"
+                        max={16}
+                        type="text"
+                        inputMode="decimal"
+                    />
+                )}
+
+                {isStringValue(parameterValue) && (
+                    <Input
+                        value={parameterValue}
+                        onChange={(val) => updateParameter(val)}
+                        className="bg-border border border-ui"
+                        max={16}
+                        placeholder="Введите текст..."
+                    />
+                )}
+
+                {isBooleanValue(parameterValue) && (
+                    <div className="w-full">
+                        <Checkbox
+                            checked={parameterValue}
+                            onChange={(checked) => updateParameter(checked)}
+                            className="bg-border border border-ui"
+                        />
+                    </div>
+                )}
+
+                <button onClick={onRemove} className="ml-auto text-gray cursor-pointer">
+                    <X size={16} />
+                </button>
+            </div>
+        </div>
+    );
+});
