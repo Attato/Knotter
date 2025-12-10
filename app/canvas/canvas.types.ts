607815846d93e9230aacba6c272cbd2699069af1
Attato <rde8519@mail.ru>
2025-12-10 22:19:17 +0300

    relocate parameter types to new parameter.types file

--- app/canvas/canvas.types.ts
+++ app/canvas/canvas.types.ts
@@ -1,4 +1,5 @@
-import { NodeShapeType } from '@/canvas/utils/nodes/getShape';
+import { type NodeShapeType } from '@/canvas/utils/nodes/getShape';
+import { ParameterType } from '@/canvas/parameter.types';
 
 export type MouseHandler = (e: MouseEvent) => void;
 
@@ -31,7 +32,7 @@ export interface Node {
     height?: number;
     position: Position;
     kind: 'node';
-    properties: PropertyType[];
+    parameters: ParameterType[];
 }
 
 export interface Edge {
@@ -42,42 +43,5 @@ export interface Edge {
     to: string;
     position: Position;
     kind: 'edge';
-    properties: PropertyType[];
-}
-
-export type ParameterType = 'number' | 'string' | 'boolean' | 'enum' | 'array';
-
-export type Enum = {
-    options: Array<{
-        id: string;
-        name: string;
-        value: string;
-    }>;
-    selectedId: string | null;
-};
-
-export type ArrayItem =
-    | { id: string; name: string; type: 'number'; value: number }
-    | { id: string; name: string; type: 'string'; value: string }
-    | { id: string; name: string; type: 'boolean'; value: boolean }
-    | { id: string; name: string; type: 'enum'; value: Enum };
-
-export type NumberConfig = {
-    base: number;
-    min: number;
-    max: number;
-    step?: number;
-};
-
-export type ParameterValue = NumberConfig | string | boolean | Enum | ArrayItem[];
-
-export interface Parameter {
-    id: string;
-    name: string;
-    type: ParameterType;
-    value: ParameterValue;
-}
-
-export interface PropertyType extends Parameter {
-    parentId: string;
+    parameters: ParameterType[];
 }
