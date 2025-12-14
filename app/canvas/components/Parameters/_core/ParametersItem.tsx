b75ac12d54f4d7c8bf3121a0f24b7ab6599e05bd
Attato <rde8519@mail.ru>
2025-12-14 19:00:41 +0300

    relocate and change BaseParameterContent to NumberParameter, StringParameter and BooleanParameter

---
+++ app/canvas/components/Parameters/_core/ParametersItem.tsx
@@ -0,0 +1,114 @@
+'use client';
+
+import { memo } from 'react';
+
+import { NumberParameter } from '@/canvas/components/Parameters/types/NumberParameter';
+import { StringParameter } from '@/canvas/components/Parameters/types/StringParameter';
+import { BooleanParameter } from '@/canvas/components/Parameters/types/BooleanParameter';
+import { EnumContent } from '@/canvas/components/Parameters/types/EnumParameter';
+import { ArrayContent } from '@/canvas/components/Parameters/types/ArrayParameter';
+
+import { useParametersItem } from '@/canvas/hooks/Parameters/_core/useParametersItem';
+
+import {
+    isNumberValue,
+    isStringValue,
+    isBooleanValue,
+    isEnumValue,
+    isArrayValue,
+} from '@/canvas/utils/parameters/parameter.type-guards';
+
+interface ParametersItemProps {
+    parameterId: string;
+    removeParameter: (parameterId: string) => void;
+}
+
+export const ParametersItem = memo(function ParametersItem({ parameterId, removeParameter }: ParametersItemProps) {
+    const {
+        parameter,
+
+        handleInputChange,
+        handleCheckboxChange,
+
+        handleUpdateParameterName,
+
+        handleUpdateCurrentValue,
+        handleUpdateMinValue,
+        handleUpdateMaxValue,
+        handleUpdateStepValue,
+
+        handleAddEnumOption,
+        handleRemoveEnumOption,
+        handleUpdateEnumOptionName,
+        handleUpdateEnumOptionValue,
+
+        handleAddArrayParameter,
+        handleRemoveArrayParameter,
+        handleUpdateArrayParameterName,
+        handleUpdateArrayParameterValue,
+    } = useParametersItem(parameterId);
+
+    if (isNumberValue(parameter)) {
+        return (
+            <NumberParameter
+                parameter={parameter}
+                handleUpdateParameterName={handleUpdateParameterName}
+                handleUpdateCurrentValue={handleUpdateCurrentValue}
+                handleUpdateMinValue={handleUpdateMinValue}
+                handleUpdateMaxValue={handleUpdateMaxValue}
+                handleUpdateStepValue={handleUpdateStepValue}
+                removeParameter={removeParameter}
+            />
+        );
+    }
+
+    if (isStringValue(parameter)) {
+        return (
+            <StringParameter
+                parameter={parameter}
+                handleInputChange={handleInputChange}
+                handleUpdateParameterName={handleUpdateParameterName}
+                removeParameter={removeParameter}
+            />
+        );
+    }
+
+    if (isBooleanValue(parameter)) {
+        return (
+            <BooleanParameter
+                parameter={parameter}
+                handleCheckboxChange={handleCheckboxChange}
+                handleUpdateParameterName={handleUpdateParameterName}
+                removeParameter={removeParameter}
+            />
+        );
+    }
+
+    if (isEnumValue(parameter)) {
+        return (
+            <EnumContent
+                parameter={parameter}
+                handleAddEnumOption={handleAddEnumOption}
+                handleRemoveEnumOption={handleRemoveEnumOption}
+                handleUpdateEnumOptionName={handleUpdateEnumOptionName}
+                handleUpdateEnumOptionValue={handleUpdateEnumOptionValue}
+                handleUpdateParameterName={handleUpdateParameterName}
+                removeParameter={removeParameter}
+            />
+        );
+    }
+
+    if (isArrayValue(parameter)) {
+        return (
+            <ArrayContent
+                parameter={parameter}
+                handleAddArrayParameter={handleAddArrayParameter}
+                handleRemoveArrayParameter={handleRemoveArrayParameter}
+                handleUpdateArrayParameterName={handleUpdateArrayParameterName}
+                handleUpdateArrayParameterValue={handleUpdateArrayParameterValue}
+                handleUpdateEnumOptionValue={handleUpdateEnumOptionValue}
+                removeParameter={removeParameter}
+            />
+        );
+    }
+});
