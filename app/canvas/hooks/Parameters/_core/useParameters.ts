2d8a92e2a1354d1d7271799bd94c101cab6f2f9c
Attato <rde8519@mail.ru>
2025-12-14 19:11:20 +0300

    change addParameter to createParameter and add updateParameter, addParameterToNode and getNodeParameters

---
+++ app/canvas/hooks/Parameters/_core/useParameters.ts
@@ -0,0 +1,83 @@
+import { useCanvasStore } from '@/canvas/store/canvasStore';
+
+import type { Parameter, ParameterType } from '@/canvas/utils/parameters/parameter.types';
+import type { Node } from '@/canvas/canvas.types';
+
+import { getNodes } from '@/canvas/utils/nodes/getNodes';
+import { createInitialParameterValue } from '@/canvas/utils/parameters/parameter.utils';
+
+import { v4 as uuid } from 'uuid';
+
+export const useParameters = () => {
+    const items = useCanvasStore((state) => state.items);
+    const parameters = useCanvasStore((state) => state.parameters);
+    const setItems = useCanvasStore((state) => state.setItems);
+    const setParameters = useCanvasStore((state) => state.setParameters);
+
+    const nodes = getNodes(items);
+
+    const createParameter = (name: string, type: ParameterType): Parameter => {
+        const newParameter: Parameter = {
+            id: uuid(),
+            name: name,
+            type,
+            data: createInitialParameterValue(type),
+        };
+
+        setParameters([...parameters, newParameter]);
+        return newParameter;
+    };
+
+    const removeParameter = (parameterId: string) => {
+        setParameters(parameters.filter((parameter) => parameter.id !== parameterId));
+    };
+
+    const updateParameter = (parameterId: string, updates: Partial<Parameter>) => {
+        setParameters(
+            parameters.map((parameter) => (parameter.id === parameterId ? { ...parameter, ...updates } : parameter)),
+        );
+    };
+
+    const addParameterToNode = (nodeId: string, parameterId: string) => {
+        const nodeIndex = items.findIndex((item) => item.kind === 'node' && item.id === nodeId);
+        const parameter = parameters.find((parameter) => parameter.id === parameterId);
+        const node = items[nodeIndex] as Node;
+        const parameterExists = node.parameters.some((parameter) => parameter.id === parameterId);
+
+        if (!parameter) return;
+        if (nodeIndex === -1) return;
+        if (parameterExists) return;
+
+        const nodeParameter: Parameter = {
+            ...parameter,
+        };
+
+        const updatedNode = {
+            ...node,
+            parameters: [...node.parameters, nodeParameter],
+        };
+
+        const updatedItems = [...items];
+        updatedItems[nodeIndex] = updatedNode;
+        setItems(updatedItems);
+    };
+
+    const getNodeParameters = (nodeId: string) => {
+        const node = nodes.find((node) => node.id === nodeId);
+
+        if (!node) return null;
+
+        return node.parameters;
+    };
+
+    return {
+        parameters,
+
+        createParameter,
+        removeParameter,
+        updateParameter,
+
+        addParameterToNode,
+        getNodeParameters,
+    };
+};
