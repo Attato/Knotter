21e8d8de3ec71fcdb3890eb1c5969834ebd7bec0
Attato <rde8519@mail.ru>
2025-12-14 18:57:33 +0300

    rename ArrayContent to ArrayParameter

---
+++ app/canvas/components/Parameters/types/ArrayParameter.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { memo } from 'react';
+
+import { EditableName } from '@/components/UI/EditableName';
+
+import { Parameter, ParameterData } from '@/canvas/utils/parameters/parameter.types';
+
+import { getDynamicIcon } from '@/canvas/utils/items/getDynamicIcon';
+import { isArrayValue } from '@/canvas/utils/parameters/parameter.type-guards';
+
+import { X } from 'lucide-react';
+interface ArrayContentProps {
+    parameter: Parameter;
+    handleAddArrayParameter: (newParameter: Parameter) => void;
+    handleRemoveArrayParameter: (parameterId: string) => void;
+    handleUpdateArrayParameterName: (parameterId: string, newName: string) => void;
+    handleUpdateArrayParameterValue: (parameterId: string, newData: ParameterData) => void;
+    handleUpdateEnumOptionValue: (optionId: string, newValue: string) => void;
+    removeParameter: (parameterId: string) => void;
+}
+
+export const ArrayContent = memo(function ArrayContent({
+    parameter,
+    handleUpdateArrayParameterName,
+    removeParameter,
+}: ArrayContentProps) {
+    const ArrayIcon = getDynamicIcon('array');
+
+    if (!isArrayValue(parameter)) return null;
+
+    return (
+        <div className="flex flex-col gap-1 px-3 py-2 bg-depth-2 text-sm rounded-md">
+            <div className="flex items-center gap-1 h-8">
+                <ArrayIcon size={16} className="min-w-4" />
+
+                <EditableName name={parameter.name} onChange={() => handleUpdateArrayParameterName} className="w-full" />
+
+                <button onClick={() => removeParameter(parameter.id)} className="ml-auto text-gray cursor-pointer">
+                    <X size={16} />
+                </button>
+            </div>
+
+            <div className="flex flex-col gap-1 border-l pl-6 border-depth-6">В разработке...</div>
+        </div>
+    );
+});
